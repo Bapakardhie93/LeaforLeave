137261caefc16a49bd307cf935102e3ff85c027e
Satriya Dwi Mahardhika <satriyamahardhika93@gmail.com>
2026-07-11 01:30:19 +0700

    Initial Commit

---
+++ LeafOrLeave/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeafOrLeave
+//
+//  Created by Bapakardhie Pacarnya Yaya on 11/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeafOrLeave/LeafOrLeaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafOrLeaveApp.swift
+//  LeafOrLeave
+//
+//  Created by Bapakardhie Pacarnya Yaya on 11/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LeafOrLeaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
